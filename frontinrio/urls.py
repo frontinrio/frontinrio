dbfbba0e597b7e2857a562bde299d1459a2f5acf
Andre Fonseca <aoqfonseca@gmail.com>
2012-04-18 15:39:17 -0300

    acertando a pagina principal

--- frontinrio/urls.py
+++ frontinrio/urls.py
@@ -1,17 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'frontinrio.views.home', name='home'),
-    # url(r'^frontinrio/', include('frontinrio.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', direct_to_template, {'template': "index.html"}),
+    url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
