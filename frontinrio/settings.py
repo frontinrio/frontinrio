6f531a85d0001148bb7baf906c8bf26c0934647a
Andre Fonseca <aoqfonseca@gmail.com>
2012-04-21 16:30:22 -0300

    colocando para a configuração de envio de email dentro do settings do projeto

--- frontinrio/settings.py
+++ frontinrio/settings.py
@@ -19,6 +19,14 @@ DATABASES = {
     }
 }
 
+## Aguardando as configurações de email
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_HOST_USER = os.environ.get("SENDGRID_USERNAME")
+# EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_PASSWORD")
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+
+
 MANAGERS = ADMINS
 
 TIME_ZONE = 'America/Sao_Paulo'
