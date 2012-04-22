2466b06bfcc66535234c5e115a26682b9adcc51c
Andre Fonseca <aoqfonseca@gmail.com>
2012-04-22 18:26:22 -0300

    colocando o campo status no model de inscricao

--- inscricao/models.py
+++ inscricao/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 
 class Inscricao(models.Model):
+    STATUS = (
+        ('p', 'Pendente'),
+        ('a', 'Aprovada'),
+        ('r', 'Rejeitada'),
+    )
+
     nome = models.CharField(max_length=255)
     cpf = models.CharField(max_length=11, unique=True)
     email = models.EmailField()
+    status = models.CharField(max_length=10, choices = STATUS)
 
 
